0cd36d94cbbc15a06c8a3075448f764903fbda2c
landaverde1196 <alejndro1196@gmail.com>
2021-02-24 19:26:20 -0600

    user-routes added and users-controllers (signUp, logIn, getUsers)

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser = require("body-parser");
 const HttpError = require("./models/http-error");
 
 const placesRoutes = require("./routes/places-routes");
+const userRoutes = require("./routes/users-routes");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
+app.use("/api/users", userRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find this route.", 404);
