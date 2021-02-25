fc95e3abc1f6134c7b4366ddb9a2197d451b49de
landaverde1196 <alejndro1196@gmail.com>
2021-02-25 17:47:12 -0600

    validating places-routes POST request

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const uuid = require("uuid").v4;
+const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 
 let DUMMY_PLACES = [
@@ -41,6 +42,11 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invaid inputs passed, please check your data.", 422);
+  }
   const { title, description, coordinates, address, creator } = req.body;
   const createdPlace = {
     id: uuid(),
