4dd6f8dc5040f64fce63749d8ef84f6e4951a1a5
landaverde1196 <alejndro1196@gmail.com>
2021-03-08 19:26:31 -0600

    code cleanup

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -3,15 +3,6 @@ const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
 
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Alejandro Landaverde",
-    email: "test@test.com",
-    password: "password",
-  },
-];
-
 const getUsers = async (req, res, next) => {
   let users;
   try {
