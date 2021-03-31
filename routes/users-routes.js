8067ccf7fa9fe0d547e6b509038934c82564e39a
landaverde1196 <alejndro1196@gmail.com>
2021-03-30 19:02:35 -0600

    using Multer to save files

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
+
 const { check } = require("express-validator");
 const usersControllers = require("../controllers/users-controllers");
+
+const fileUpload = require("../middleware/file-upload");
 
 router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single('image'),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
