007f558a6a9b11ffac8f89cf57930a41e84b721f
landaverde1196 <alejndro1196@gmail.com>
2021-04-07 18:47:24 -0600

    using ENV variables in Backend

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://landaverde:Bruce1996@cluster0.xdlli.mongodb.net/spots?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xdlli.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
