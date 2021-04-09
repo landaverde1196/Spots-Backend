0593a960018bcc969f6d0c8be323eb321705de3c
Daniel Le'Sage <daniellesage25@gmail.com>
2021-04-09 17:00:18 -0600

    fixing shit

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,24 +1,26 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const { check } = require("express-validator");
-const usersControllers = require("../controllers/users-controllers");
+const { check } = require('express-validator');
+const usersControllers = require('../controllers/users-controllers');
 
-const fileUpload = require("../middleware/file-upload");
+const fileUpload = require('../middleware/file-upload');
+const multer = require('multer');
 
-router.get("/", usersControllers.getUsers);
+router.get('/', usersControllers.getUsers);
 
 router.post(
-  "/signup",
-  fileUpload.single('image'),
-  [
-    check("name").not().isEmpty(),
-    check("email").normalizeEmail().isEmail(),
-    check("password").isLength({ min: 6 }),
-  ],
-  usersControllers.signup
+    '/signup',
+    // fileUpload.single('image'),
+    multer().single('image'),
+    [
+        check('name').not().isEmpty(),
+        check('email').normalizeEmail().isEmail(),
+        check('password').isLength({ min: 6 }),
+    ],
+    usersControllers.signup
 );
 
-router.post("/login", usersControllers.login);
+router.post('/login', usersControllers.login);
 
 module.exports = router;
